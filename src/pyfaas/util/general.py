9e1c7f21556f1a66ab62770699f63f1bb7547aab
marcoimbee <marcoimbelli@yahoo.it>
2026-09-15 19:11:03 +0200

    fix: fix error message in checking timeout field

--- src/pyfaas/util/general.py
+++ src/pyfaas/util/general.py
@@ -19,7 +19,7 @@ def read_config_toml(path: str) -> dict:
     
     # Checking receive timeout field
     if type(config['network']['receive_timeout_s']) is not int:
-        raise Exception(f"Config error: field 'receive_timeout_s' must be of type 'str', while '{type(config['network']['receive_timeout_s'])}' was provided")
+        raise Exception(f"Config error: field 'receive_timeout_s' must be of type 'int', while '{type(config['network']['receive_timeout_s'])}' was provided")
     if config['network']['receive_timeout_s'] < 0:
         raise Exception(f"Config error: invalid value {config['network']['receive_timeout_s']} for field 'receive_timeout_s'")
 
