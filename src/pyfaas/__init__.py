e1e1a31b7d2ae4fd4396071204dc75b984cc3715
marcoimbee <marcoimbelli@yahoo.it>
2026-09-16 19:52:08 +0200

    fix: basic bug fixes

--- src/pyfaas/__init__.py
+++ src/pyfaas/__init__.py
@@ -1,7 +1,7 @@
 from .pyfaas import pyfaas_exec
 from .pyfaas import pyfaas_ping
-from .pyfaas import pyfaas_get_stats
 from .pyfaas import pyfaas_config
+from .pyfaas import pyfaas_close
 from .pyfaas import pyfaas_register
 from .pyfaas import pyfaas_unregister
 from .pyfaas import pyfaas_list
@@ -10,18 +10,20 @@ from .pyfaas import pyfaas_get_worker_info
 from .pyfaas import pyfaas_get_cache_dump
 from .pyfaas import pyfaas_load_workflow
 from .pyfaas import pyfaas_chain_exec
+from .pyfaas import pyfaas_get_worker_ids
 
 __all__ = [
     'pyfaas_exec',
     'pyfaas_config',
+    'pyfaas_close',
     'pyfaas_ping',
     'pyfaas_get_stats',
     'pyfaas_register',
     'pyfaas_unregister',
     'pyfaas_list',
-    'pyfaas_get_stats',
     'pyfaas_get_worker_info',
     'pyfaas_get_cache_dump',
     'pyfaas_load_workflow',
-    'pyfaas_chain_exec'
+    'pyfaas_chain_exec',
+    'pyfaas_get_worker_ids'
 ]
